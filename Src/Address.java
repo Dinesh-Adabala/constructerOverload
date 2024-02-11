fda966a977e6ca9afa9f710c6b9bf39e7392ae21
Dinesh Adabala <dinesh.adabala417@gmail.com>
2024-02-11 09:41:10 +0530

    hai hello Address

---
+++ Src/Address.java
@@ -0,0 +1,9 @@
+public class Address {
+    private String pincode;
+    private String city;
+
+    public Address(String pincode, String city) {
+        this.pincode = pincode;
+        this.city = city;
+    }
+}
